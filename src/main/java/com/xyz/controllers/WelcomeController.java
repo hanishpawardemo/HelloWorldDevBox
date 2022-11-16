63521877a2dd34c951199fc0367d26c1d4ece0b9
Ana <ana.rusucibc.com>
2022-11-15 21:06:25 -0500

    Added Integration Test

--- src/main/java/com/xyz/controllers/WelcomeController.java
+++ src/main/java/com/xyz/controllers/WelcomeController.java
@@ -1,15 +1,21 @@
 package com.xyz.controllers;
 
-import org.springframework.stereotype.Controller;
+import com.xyz.dto.WelcomeDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping(value = "/ebm-demo/api/v1/json")
 public class WelcomeController {
-	
-	@RequestMapping(method = RequestMethod.GET, produces = {"application/json"})
-	public @ResponseBody String helloWorld() {
-		return "Hello World!!!";
+
+	@GetMapping(value = "/welcomeMessage")
+	public ResponseEntity<WelcomeDto> helloWorld() {
+		WelcomeDto welcomeDto = new WelcomeDto();
+		welcomeDto.setMessage1("Hello World !!!!!!");
+		welcomeDto.setMessage2("Test API for demo");
+		return new ResponseEntity<WelcomeDto>(welcomeDto,HttpStatus.OK);
 	}
 }
---
+++ src/test/java/ebm_demo/WelcomeIntegrationTest.java
@@ -0,0 +1,44 @@
+package ebm_demo;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+classes = {com.xyz.Application.class})
+public class WelcomeIntegrationTest {
+
+    @LocalServerPort
+    private int localPort;
+
+    private HttpHeaders headers;
+    private TestRestTemplate restTemplate;
+
+    private String url = "/ebm-demo/api/v1/json/welcomeMessage";
+
+    @Before
+    public void setup(){
+        restTemplate = new TestRestTemplate();
+        headers = new HttpHeaders();
+    }
+
+    @Test
+    public void testHelloWorldAPI(){
+        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+        ResponseEntity<String> response = restTemplate.exchange(createURLWithPort(url), HttpMethod.GET,
+                entity, String.class);
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
+        System.out.println(response.getBody());
+    }
+
+    private String createURLWithPort(String uri) {
+        return "http://localhost:" + localPort + uri;
+    }
+}
